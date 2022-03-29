8c40172f23e344fe44d09a4d37fbf62e7c56a19a
Sampad Sikder <sampadsikder84@gmail.com>
2022-03-29 19:57:47 +0600

    Dividing matrices using divide and conquer

--- strassen_multiplication.cpp
+++ strassen_multiplication.cpp
@@ -6,6 +6,28 @@ int max_of_two(int n1, int n2)
 {
     return n1 > n2 ? n1 : n2;
 }
+
+void MatrixAdd(double **A, double **B, double **result, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i][j] = A[i][j] + B[i][j];
+        }
+    }
+}
+
+void MatrixSubtrac(double **A, double **B, double **result, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i][j] = A[i][j] - B[i][j];
+        }
+    }
+}
 void strassen(double **A, double **B, double **C, int n)
 {
     if (n == 1)
@@ -16,7 +38,51 @@ void strassen(double **A, double **B, double **C, int n)
     else
     {
         int divide = (n / 2);
-        double A11[divide][divide];
+        double *A11[divide], *A12[divide], *A21[divide], *A22[divide];
+        double *B11[divide], *B12[divide], *B21[divide], *B22[divide];
+        double *C11[divide], *C12[divide], *C21[divide], *C22[divide];
+        double *AResult[divide], *BResult[divide];
+        double *P1[n], *P2[n], *P3[n], *P4[n], *P5[n], *P6[n], *P7[n];
+
+        for (int i = 0; i < divide; i++)
+        {
+            A11[i] = (double *)malloc(divide * sizeof(double));
+            A12[i] = (double *)malloc(divide * sizeof(double));
+            A21[i] = (double *)malloc(divide * sizeof(double));
+            A22[i] = (double *)malloc(divide * sizeof(double));
+            B11[i] = (double *)malloc(divide * sizeof(double));
+            B12[i] = (double *)malloc(divide * sizeof(double));
+            B21[i] = (double *)malloc(divide * sizeof(double));
+            B22[i] = (double *)malloc(divide * sizeof(double));
+        }
+        for (int i = 0; i < n; i++)
+        {
+            P1[i] = (double *)malloc(n * sizeof(double));
+            P2[i] = (double *)malloc(n * sizeof(double));
+            P3[i] = (double *)malloc(n * sizeof(double));
+            P4[i] = (double *)malloc(n * sizeof(double));
+            P5[i] = (double *)malloc(n * sizeof(double));
+        }
+        for (int i = 0; i < divide; i++)
+        {
+            for (int j = 0; j < divide; j++)
+            {
+                A11[i][j] = A[i][j];
+                A12[i][j] = A[i][j + divide];
+                A21[i][j] = A[i + divide][j];
+                A22[i][j] = A[i + divide][j + divide];
+
+                B11[i][j] = B[i][j];
+                B12[i][j] = B[i][j + divide];
+                B21[i][j] = B[i + divide][j];
+                B22[i][j] = B[i + divide][j + divide];
+            }
+        }
+        MatrixAdd(A11, A22, AResult, divide);
+        MatrixAdd(B11, B22, BResult, divide);   // b11 + b22
+        strassen(AResult, BResult, P1, divide); // p1 = (a11+a22) * (b11+b22)
+
+        MatrixAdd(A21, A22, AResult, divide);
     }
 }
 void print_matrix(double V[][N], int m, int n)
